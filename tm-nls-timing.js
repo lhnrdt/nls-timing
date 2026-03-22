d048d13f4003454155562647a9e992c97e472055
s4jolehn <s4jolehn@uni-trier.de>
2026-03-22 11:58:09 +0100

    update name

--- tm-nls-timing.js
+++ tm-nls-timing.js
@@ -1,19 +1,21 @@
 // ==UserScript==
-// @name         YT NLS Stable Table + Relative Window 20260321
-// @namespace    clean.test.local
+// @name         YouTube NLS Timing Overlay
+// @namespace    https://github.com/lhnrdt/nls-timing
 // @version      1.1
 // @description  Stable gated NLS overlay with relative timing window
 // @match        https://www.youtube.com/*
 // @grant        none
 // @run-at       document-idle
-// @require      https://raw.githubusercontent.com/<user>/<repo>/main/src/config.js
-// @require      https://raw.githubusercontent.com/<user>/<repo>/main/src/state.js
-// @require      https://raw.githubusercontent.com/<user>/<repo>/main/src/utils.js
-// @require      https://raw.githubusercontent.com/<user>/<repo>/main/src/track-model.js
-// @require      https://raw.githubusercontent.com/<user>/<repo>/main/src/overlay-main.js
-// @require      https://raw.githubusercontent.com/<user>/<repo>/main/src/overlay-relative.js
-// @require      https://raw.githubusercontent.com/<user>/<repo>/main/src/track-map.js
-// @require      https://raw.githubusercontent.com/<user>/<repo>/main/src/settings.js
-// @require      https://raw.githubusercontent.com/<user>/<repo>/main/src/socket.js
-// @require      https://raw.githubusercontent.com/<user>/<repo>/main/src/app.js
+// @updateURL    https://raw.githubusercontent.com/lhnrdt/nls-timing/modularization/tm-nls-timing.js
+// @downloadURL  https://raw.githubusercontent.com/lhnrdt/nls-timing/modularization/tm-nls-timing.js
+// @require      https://raw.githubusercontent.com/lhnrdt/nls-timing/modularization/src/config.js
+// @require      https://raw.githubusercontent.com/lhnrdt/nls-timing/modularization/src/state.js
+// @require      https://raw.githubusercontent.com/lhnrdt/nls-timing/modularization/src/utils.js
+// @require      https://raw.githubusercontent.com/lhnrdt/nls-timing/modularization/src/track-model.js
+// @require      https://raw.githubusercontent.com/lhnrdt/nls-timing/modularization/src/overlay-main.js
+// @require      https://raw.githubusercontent.com/lhnrdt/nls-timing/modularization/src/overlay-relative.js
+// @require      https://raw.githubusercontent.com/lhnrdt/nls-timing/modularization/src/track-map.js
+// @require      https://raw.githubusercontent.com/lhnrdt/nls-timing/modularization/src/settings.js
+// @require      https://raw.githubusercontent.com/lhnrdt/nls-timing/modularization/src/socket.js
+// @require      https://raw.githubusercontent.com/lhnrdt/nls-timing/modularization/src/app.js
 // ==/UserScript==
